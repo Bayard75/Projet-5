fd837c1a9ade65b71aea90a33a001977f221a00f
Bayardd <bayard.maniraho@gmail.com>
2019-07-01 20:41:13 +0200

    minor changes in structure

--- files/Aliment.sql
+++ files/Aliment.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS aliment (
     id_aliment SMALLINT AUTO_INCREMENT NOT NULL,
     name_aliment VARCHAR(150) NOT NULL,
+    substitut_of VARCHAR(150),
     category SMALLINT UNSIGNED NOT NULL,
     store TEXT,
     grade TEXT NOT NULL,
     description TEXT,
     link TEXT,
+    favorite TINYINT,
     PRIMARY KEY (id_aliment),
     CONSTRAINT fk_category FOREIGN KEY(category) REFERENCES Category(id_category),
-    UNIQUE INDEX (name_aliment, category,grade(1)),
-    UNIQUE INDEX(name_aliment)
+    UNIQUE INDEX (name_aliment)
 )
 ENGINE = InnoDB;
